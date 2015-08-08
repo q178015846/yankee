6847c967225f42ed35c4ad3bb737812ccb5a6989
Rock <178015846@qq.com>
2015-08-08 13:27:22 +0800

    init

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.beyankee.com/yankee/admin/');
+define('HTTP_CATALOG', 'http://www.beyankee.com/yankee/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.beyankee.com/yankee/admin/');
+define('HTTPS_CATALOG', 'http://www.beyankee.com/yankee/');
+
+// DIR
+define('DIR_APPLICATION', '/home/wwwroot/default/yankee/admin/');
+define('DIR_SYSTEM', '/home/wwwroot/default/yankee/system/');
+define('DIR_LANGUAGE', '/home/wwwroot/default/yankee/admin/language/');
+define('DIR_TEMPLATE', '/home/wwwroot/default/yankee/admin/view/template/');
+define('DIR_CONFIG', '/home/wwwroot/default/yankee/system/config/');
+define('DIR_IMAGE', '/home/wwwroot/default/yankee/image/');
+define('DIR_CACHE', '/home/wwwroot/default/yankee/system/cache/');
+define('DIR_DOWNLOAD', '/home/wwwroot/default/yankee/system/download/');
+define('DIR_UPLOAD', '/home/wwwroot/default/yankee/system/upload/');
+define('DIR_LOGS', '/home/wwwroot/default/yankee/system/logs/');
+define('DIR_MODIFICATION', '/home/wwwroot/default/yankee/system/modification/');
+define('DIR_CATALOG', '/home/wwwroot/default/yankee/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'i7jhcev21t');
+define('DB_DATABASE', 'yankee');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'yk_');
