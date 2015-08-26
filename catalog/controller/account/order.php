67037b180876fe97fdef209b45def22c1af4a9bc
Rock <178015846@qq.com>
2015-08-26 13:52:20 +0800

    fix

--- catalog/controller/account/order.php
+++ catalog/controller/account/order.php
@@ -366,12 +366,18 @@ class ControllerAccountOrder extends Controller {
 
 			$data['continue'] = $this->url->link('account/order', '', 'SSL');
 
+			//加载微信JS-SDK
+			$this->load->library('wxapi');
+			$this->wx = new Wxapi();
+			$signPackage = $this->wx->GetSignPackage();
+
 			$data['column_left'] = $this->load->controller('common/column_left');
 			$data['column_right'] = $this->load->controller('common/column_right');
 			$data['content_top'] = $this->load->controller('common/content_top');
 			$data['content_bottom'] = $this->load->controller('common/content_bottom');
 			$data['footer'] = $this->load->controller('common/footer');
 			$data['header'] = $this->load->controller('common/header');
+			$data['signPackage'] = $signPackage;
 
 			if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/account/order_info.tpl')) {
 				$this->response->setOutput($this->load->view($this->config->get('config_template') . '/template/account/order_info.tpl', $data));
@@ -411,10 +417,7 @@ class ControllerAccountOrder extends Controller {
 
 			$data['continue'] = $this->url->link('account/order', '', 'SSL');
 
-			//加载微信JS-SDK
-			$this->load->library('wxapi');
-			$this->wx = new Wxapi();
-			$signPackage = $this->wx->GetSignPackage();
+			
 
 			$data['column_left'] = $this->load->controller('common/column_left');
 			$data['column_right'] = $this->load->controller('common/column_right');
@@ -422,7 +425,7 @@ class ControllerAccountOrder extends Controller {
 			$data['content_bottom'] = $this->load->controller('common/content_bottom');
 			$data['footer'] = $this->load->controller('common/footer');
 			$data['header'] = $this->load->controller('common/header');
-			$data['signPackage'] = $signPackage;
+		
 
 			if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/error/not_found.tpl')) {
 				$this->response->setOutput($this->load->view($this->config->get('config_template') . '/template/error/not_found.tpl', $data));
--- catalog/view/theme/yankee/template/account/order_info.tpl
+++ catalog/view/theme/yankee/template/account/order_info.tpl
@@ -161,7 +161,7 @@
       <?php } ?>
       <div class="buttons clearfix">
         <div class="pull-right"><a href="<?php echo $continue; ?>" class="btn btn-primary"><?php echo $button_continue; ?></a></div>
-        <div><button onclick="sharetimeline()" class="btn btn-primary">分享</button></div>
+        <div><button id="sharetimeline" class="btn btn-primary">分享</button></div>
       </div>
       <?php echo $content_bottom; ?></div>
     <?php echo $column_right; ?></div>
@@ -181,7 +181,20 @@
     ]
   });
 
-  function sharetimeline () {
+  $("#sharetimeline").click(function () {
+    // body...
+      wx.scanQRCode({
+        needResult: 0, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+        scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
+        success: function (res) {
+        var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
+        },
+        fail:function (res) {
+            // body...
+        }
+    });
+  });
+ /* function sharetimeline () {
     // body...
     wx.scanQRCode({
         needResult: 0, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
@@ -193,7 +206,7 @@
             // body...
         }
     });
-  }
+  }*/
   
 </script>
 <?php echo $footer; ?>
