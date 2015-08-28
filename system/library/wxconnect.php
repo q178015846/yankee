32989beac8bdf2ca9be7e51973c0aebfc2389fe1
Rock <178015846@qq.com>
2015-08-28 09:48:30 +0800

    fIx

--- system/library/wxconnect.php
+++ system/library/wxconnect.php
@@ -16,6 +16,39 @@ class Wxconnect
         }
     }
 
+
+    //关注后答复
+    public function responseMsg()
+    {
+        //get post data, May be due to the different environments  
+        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+          //extract post data  
+        if (!empty ($postStr)) {
+            $postObj = simplexml_load_string($postStr,'SimpleXMLElement',LIBXML_NOCDATA);
+            $fromUsername = $postObj->FromUserName;  
+            $toUsername = $postObj->ToUserName;  
+            $keyword = trim($postObj->Content);  
+            $Event = trim($postObj->Event);  
+            $time = time();  
+            $textTpl = "<xml>  
+                        <ToUserName><![CDATA[%s]]></ToUserName>  
+                        <FromUserName><![CDATA[%s]]></FromUserName>  
+                        <CreateTime>%s</CreateTime>  
+                        <MsgType><![CDATA[%s]]></MsgType>  
+                        <Content><![CDATA[%s]]></Content>  
+                        <FuncFlag>0</FuncFlag>  
+                        </xml>"; 
+        }
+
+         //测试事件推送  
+        if($Event=="subscribe"){  
+            $contentStr = "欢迎关注YANKEE CANDLE香薰体验馆公众平台!";  
+            $msgType = "text";  
+            $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);  
+            echo $resultStr;  
+        } 
+    }
+
     private function checkSignature()
     {
        // you must define TOKEN by yourself
