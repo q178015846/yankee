4eddadb830fffca4cfb6b84b3c80413fca7f1e5e
rock <178015846@qq.com>
2015-09-04 10:36:54 +0800

    fix

--- admin/model/sale/order.php
+++ admin/model/sale/order.php
@@ -143,7 +143,7 @@ class ModelSaleOrder extends Model {
 				'accept_language'         => $order_query->row['accept_language'],
 				'date_added'              => $order_query->row['date_added'],
 				'date_modified'           => $order_query->row['date_modified'],
-				'shipping_order_code'	  => $order_query->row['date_modified']
+				'shipping_order_code'	  => $order_query->row['shipping_order_code']
 			);
 		} else {
 			return;
