b7e80a5047ff67e7934e418bc54ac0c9505a9973
Rock <178015846@qq.com>
2015-09-14 17:06:29 +0800

    fix

--- catalog/language/zh-CN/module/isotope.php
+++ catalog/language/zh-CN/module/isotope.php
@@ -5,9 +5,9 @@ $_['heading_title'] = 'Isotope';
 // Text
 $_['text_tax']      = 'Ex Tax:';
 
-$_['filter_title_all'] = 'All';
+$_['filter_title_all'] = '全部商品';
 $_['filter_title_latest'] = '新品上市';
 $_['filter_title_bestseller'] = '热销产品';
-$_['filter_title_popular'] = 'Popular';
+$_['filter_title_popular'] = '流行商品';
 $_['filter_title_special'] = '热销中...';
-$_['filter_title_featured'] = 'Featured';
+$_['filter_title_featured'] = '特色商品';
--- catalog/language/zh-CN/module/upsells.php
+++ catalog/language/zh-CN/module/upsells.php
@@ -1,6 +1,6 @@
 <?php
 // Heading 
-$_['heading_title']  = 'Also bought products';
-$_['empty_products']  = 'There are no products linked with this product.';
+$_['heading_title']  = '猜您喜欢';
+$_['empty_products']  = '暂无相关产品';
 $_['tab_also_bought_title']  = '猜您喜欢';
 $_['text_view']  = '查看';
